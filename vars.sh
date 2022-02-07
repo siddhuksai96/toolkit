d838b6d0ddb0912ae6b26d3cb5cd6fd0c705ea6c
siddharthak <siddhuksai@gmail.com>
2022-02-06 21:16:07 -0500

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -8,3 +8,4 @@ alias cfl='cf login -a api.g1.app.cloud.comcast.net'
 alias jtd="jq -R 'split(\".\") | .[1] | @base64d | fromjson' <<<"
 alias zipLambda='pip3 install -r requirements.txt --target . && zip -r ${PWD##*/}.zip . -x .\* _\*'
 alias makeVenv='python3 -m venv env && source env/bin/activate'
+alias mvn8="JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_311.jdk/Contents/Home/ && mvn"
